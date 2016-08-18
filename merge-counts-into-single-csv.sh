b688f9faf8f8b2800765fd9084dbc735d4a25438
Ken'ichi Ohmichi <ken1ohmichi@gmail.com>
2016-08-18 10:08:23 +0900

    Implementing merge-counts-into-single-csv.sh

--- merge-counts-into-single-csv.sh
+++ merge-counts-into-single-csv.sh
@@ -2,9 +2,20 @@
 
 cd `dirname $0`
 
-DIRS=`find . -type d`
+declare -A COUNTS
+
+DIRS=`find . -type d | grep -E '[0-9]{8}' | sed s@\.\/@@ | sort`
 for dir in $DIRS
 do
-	echo $dir
+	COUNTS["DATE"]+="$dir, "
+	for STATUS in "NEW" "INCOMPLETE" "CONFIRMED" "TRIAGED" "INPROGRESS" "FIXCOMMITTED"
+	do
+		COUNTS[$STATUS]+=`grep $STATUS $dir/result.csv | awk -F"," '{print $2}'`
+		COUNTS[$STATUS]+=', '
+	done
 done
 
+for KEY in "DATE" "NEW" "INCOMPLETE" "CONFIRMED" "TRIAGED" "INPROGRESS" "FIXCOMMITTED"
+do
+	echo "$KEY, ${COUNTS[$KEY]}"
+done
