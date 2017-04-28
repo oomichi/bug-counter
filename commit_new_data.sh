301e745a01782ec7d5f77ccaeff93f4501e3ee9e
Ken'ichi Ohmichi <ken1ohmichi@gmail.com>
2017-04-29 07:40:43 +0900

    Make graph process auto

--- commit_new_data.sh
+++ commit_new_data.sh
@@ -1,6 +1,28 @@
+DOCKER_IMAGE="test/graph"
+DOCKER_WORKER="make_graph"
+
 git checkout master
 git pull
 ./merge-counts-into-single-csv.sh
 git add data/
 git commit -m "Add new data"
 git push origin master
+
+# Create a new graph
+if -z `docker ps -a | grep ${DOCKER_WORKER}` ; then
+    if -z `docker images | grep ${DOCKER_IMAGE}` ; then
+        cd graph
+        docker build -t ${DOCKER_IMAGE} .
+        cd ..
+    fi
+    docker run --name ${DOCKER_WORKER} ${DOCKER_IMAGE}
+fi
+
+docker cp result.csv ${DOCKER_WORKER}:result.csv
+docker restart make_graph
+rm tempest_bug_count.png
+docker cp make_graph:tempest_bug_count.png tempest_bug_count.png
+git add tempest_bug_count.png
+git commit -m "Add new graph"
+git push origin master
+
